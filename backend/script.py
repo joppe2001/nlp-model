6c77be4f6505c71ad61b3d3e9d857cd0e1fbf509
joppe2001 <joppe.montezinos@gmail.com>
2023-11-09 16:03:43 +0100

    new commit

--- backend/script.py
+++ backend/script.py
@@ -3,13 +3,11 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import pickle
 from preprocessing import preprocess_review
-# import flask cors
 
 app = Flask(__name__)
-# flask cors to allow cross origin requests
 cors = CORS(app)
 
-# Load the pre-trained model and CountVectorizer
+
 model = pickle.load(open('model.pkl', 'rb'))
 cv = pickle.load(open('cv.pkl', 'rb'))
 
@@ -19,7 +17,7 @@ def home():
 
 @app.route('/api/predict', methods=['POST'])
 def predict():
-    data = request.get_json(force=True)  # Get data sent in JSON format
+    data = request.get_json(force=True)
     review_text = data['review']
     review_processed = preprocess_review(review_text)
     review_vectorized = cv.transform([review_processed]).toarray()
